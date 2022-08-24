9ed0a2ad50cac55ac7f59e39ffd359c0640a49be
mechanicworld <47930174+mechanicworld@users.noreply.github.com>
2022-08-24 22:54:21 +0300

    Initialize Layout of  logo and new record button

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
+import "./App.scss";
 
-import './App.scss'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
 
+import logo from "./assets/img/tesodev-logo.png";
+import Button from "./components/utils/Buttons/Button/Button";
 
 function App() {
-
   return (
-    <div className=''>
-     <h1>Home Page</h1>
+    <div className="">
+      <div className="new-record-container">
+        <Button buttonName={"Add new record"} />
+      </div>
+      <div className="container-center">
+        <div>
+          <img className="logo" src={logo} alt="tesodev-logo" />
+          <div className="logo-subtitle">Search app</div>
+        </div>
+        <div>
+        <p className="search-title">Find in records</p>
+
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
