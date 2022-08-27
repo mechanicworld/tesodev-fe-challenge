11dfeaafb5baf4a83542579123a3548226c7a032
mechanicworld <47930174+mechanicworld@users.noreply.github.com>
2022-08-27 16:47:17 +0300

    Wrapped with Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.scss";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import SearchResults from './pages/SearchResults/SearchResults';
-import AddRecords from './pages/AddRecords/AddRecords';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchResults from "./pages/SearchResults/SearchResults";
+import AddRecords from "./pages/AddRecords/AddRecords";
+import { RecordProvider } from "./context";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App/>}/>
-      <Route path='/search-results' element={<SearchResults/>}/>
-      <Route path='/add-records' element={<AddRecords/>}/>      
-    </Routes>
-    </BrowserRouter>   
+    <RecordProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/search-results" element={<SearchResults />} />
+          <Route path="/add-records" element={<AddRecords />} />
+        </Routes>
+      </BrowserRouter>
+    </RecordProvider>
   </React.StrictMode>
-)
+);
