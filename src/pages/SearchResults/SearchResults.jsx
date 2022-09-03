8444831e19ec86c8d610b824df2ad8c5bafdb2bc
mechanicworld <47930174+mechanicworld@users.noreply.github.com>
2022-09-04 00:17:51 +0300

    Update Search Results for Pagination

--- src/pages/SearchResults/SearchResults.jsx
+++ src/pages/SearchResults/SearchResults.jsx
@@ -10,7 +10,7 @@ import sortArrayByOrder from "../../components/utils/helpers/sortArrayByOrder";
 import PaginationBar from "../../components/PaginationBar/PaginationBar";
 
 function SearchResults() {
-  const [dropDownOn, setDropDownOn] = useState(1);
+  const [dropDownOn, setDropDownOn] = useState(0);
 
   const {
     columns,
@@ -20,36 +20,16 @@ function SearchResults() {
     filteredRecords,
     orderByList,
     orderById,
+    currentPage,
+    pageList,
     dispatch,
   } = useRecords();
   let navigate = useNavigate();
-
-  // const sortArrayByOrder2 = (orderId, array) => {
-  //   let sortedArray = [];
-  //   sortedArray = array.sort((a, b) => {
-  //     let first = a[orderById];
-  //     let second = b[orderById];
-  //     if (first < second) {
-  //       return -1;
-  //     } else if (first > second) {
-  //       return 1;
-  //     }
-  //     return 0;
-  //   });
-  // };
-  // useEffect(() => {
-  //   if (filteredRecords !== undefined) {
-  //     let type = [1, 3].includes(orderById) ? "DESC" : "ASC";
-  //     let order = [0, 1].includes(orderById) ? 0 : 3;
-  //     let final = sortArrayByOrder(order, filteredRecords, type);
-  //     console.log(final);
-  //   }
-  // }, [orderById]);
-
+  const orderDropDown = useRef(null);
+  const orderBox = useRef(null);
   const handleSearchList = useCallback(
     (e) => {
       e.preventDefault();
-      // setSearchValue(e.target.value);
       dispatch({
         type: "SET_SEARCH",
         value: e.target.value,
@@ -63,8 +43,8 @@ function SearchResults() {
   };
   const handleOrderById = (e, index) => {
     e.preventDefault();
-    console.log(index);
-    setDropDownOn(x => !x)
+
+    setDropDownOn((x) => !x);
     dispatch({
       type: "UPDATE_ORDER",
       value: index,
@@ -75,35 +55,62 @@ function SearchResults() {
     e.preventDefault();
     navigate("../search-results");
   };
+  const navigateAddRecords = (e) => {
+    e.preventDefault()
+    navigate('../add-records')
+  }
 
   useEffect(() => {
+    if (records != undefined) {
+      let filtered = records?.filter((record) => {
+        if (
+          record[searchCategoryId].toLowerCase().includes(search.toLowerCase())
+        ) {
+          return record;
+        }
+      });
+      let type = [1, 3].includes(orderById) ? "DESC" : "ASC";
+      let order = [0, 1].includes(orderById) ? 0 : 3;
+      let ordered = sortArrayByOrder(order, filtered, type);
 
-    let filtered = records.filter((record, index) => {
-      if (
-        record[searchCategoryId].toLowerCase().includes(search.toLowerCase())
-      ) {
-        return record;
-      }
-    })
-    let type = [1, 3].includes(orderById) ? "DESC" : "ASC";
-    let order = [0, 1].includes(orderById) ? 0 : 3;
-    let ordered = sortArrayByOrder(order, filtered, type);
-    console.log(ordered)
-    dispatch({
-      type: "UPDATE_FILTERED_RECORD",
-      value: ordered
-    });
-    // Add Filtered
-    
-  }, [search,orderById]);
+      dispatch({
+        type: "UPDATE_FILTERED_RECORD",
+        value: ordered,
+      });
+      dispatch({
+        type: "UPDATE_PAGE_LIST",
+        value: [...Array(Math.ceil(ordered.length / 5)).keys()],
+      });
+    }
+  }, [search, orderById]);
 
   const styleCondition = (categoryId) =>
     searchCategoryId == categoryId
       ? style.searchBarListSearchedItem
       : style.searchBarListItem;
 
+  const handleClick = (e) => {
+    e.preventDefault();
+// Recursive Todo for children item
+  //   const childrenList = (element, list) => {
+  //     console.log(list);
+
+  //     if (element.childElementCount > 0) {
+  //       let listTemp = [];
+  //       [...element.children].map((each) => {
+  //         listTemp.push(each);
+  //         console.log(listTemp, list);
+  //         childrenList(each, list.concat(listTemp));
+  //       });
+  //       return listTemp.concat(list);
+  //     } else {
+  //       return list.concat(element);
+  //     }
+  //   };
+  //   console.log(childrenList(orderBox.current, []));
+  };
   return (
-    <div className={style.page}>
+    <div onClick={handleClick} className={style.page}>
       <div className={style.topContainer}>
         <img src={image} alt="tesodev" />
         <SearchBox
@@ -111,58 +118,62 @@ function SearchResults() {
           navigateSearchResult={navigateSearchResult}
           search={search}
         />
-        <Button buttonName={"Add new record"} />
+        <Button onClick={navigateAddRecords} buttonName={"Add new record"} />
       </div>
       <div className={style.resultContainer}>
         <div className={style.resultListContainer}>
           <div className={style.resultList}>
             <ul className={style.listBox}>
-              {filteredRecords?.slice().map((res, index) => {
-                return (
-                  <>
-                    <li key={index} className={style.listItem}>
-                      <div className={style.listItemInsideBox}>
-                        <div className={style.listItemFirstLine}>
-                          <span className={styleCondition(0)}>{res[0]}</span>
-                          <span> - </span>
-                          <span className={styleCondition(1)}>{res[1]}</span>
-                          <span
-                            className={`${styleCondition(3)} ${style.floatR}`}
-                          >
-                            {res[3]}
-                          </span>
-                        </div>
-                        <div>
-                          <div
-                            className={`${styleCondition(4)} ${style.floatL}`}
-                          >
-                            {res[2]}
+              {filteredRecords
+                ?.slice(currentPage * 5, (currentPage + 1) * 5)
+                .map((res, index) => {
+                  return (
+                    <>
+                      <li key={index} className={style.listItem}>
+                        <div className={style.listItemInsideBox}>
+                          <div className={style.listItemFirstLine}>
+                            <span className={styleCondition(0)}>{res[0]}</span>
+                            <span> - </span>
+                            <span className={styleCondition(1)}>{res[1]}</span>
+                            <span
+                              className={`${styleCondition(3)} ${style.floatR}`}
+                            >
+                              {res[3]}
+                            </span>
                           </div>
-                          <div
-                            className={`${styleCondition(5)} ${style.floatR}`}
-                          >
-                            {res[4]} <span>-</span> {res[5]}
+                          <div>
+                            <div
+                              className={`${styleCondition(4)} ${style.floatL}`}
+                            >
+                              {res[2]}
+                            </div>
+                            <div
+                              className={`${styleCondition(5)} ${style.floatR}`}
+                            >
+                              {res[4]} <span>-</span> {res[5]}
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    </li>
-                    <div className={style.seperatorLine}></div>
-                  </>
-                );
-              })}
+                      </li>
+                      <div className={style.seperatorLine}></div>
+                    </>
+                  );
+                })}
             </ul>
           </div>
-          <div className={style.paginationBox}>
-            <PaginationBar/>
-          </div>
+          {pageList.length > 0 && (
+            <div className={style.paginationBox}>
+              <PaginationBar />
+            </div>
+          )}
         </div>
-        <div className={style.orderBox}>
+        <div ref={orderBox} className={style.orderBox}>
           <button className={style.sortButton} onClick={handleDropDownList}>
             <img src={sortIcon} alt="" />
             Order By
           </button>
-          {dropDownOn && (
-            <div className={style.dropDownBox}>
+          {dropDownOn == 1 && (
+            <div ref={orderDropDown} className={style.dropDownBox}>
               {orderByList && (
                 <ul className={style.listBox}>
                   {orderByList.map((orderItem, index) => (
