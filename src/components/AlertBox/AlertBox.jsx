25aed8f16f61e00eaf34066ccf753d1f5eb5e0f7
mechanicworld <47930174+mechanicworld@users.noreply.github.com>
2022-09-05 00:15:11 +0300

    Update alert message

--- src/components/AlertBox/AlertBox.jsx
+++ src/components/AlertBox/AlertBox.jsx
@@ -3,14 +3,12 @@ import { useEffect } from 'react'
 import style from './AlertBox.module.scss'
 function AlertBox({alert,errorStatus,index}) {
   const messages ={
-    nameSurname: "Name and surname should contain at least 2 words and ma",
+    nameSurname: "Name and surname should contain at least 2 words and total length should be between 2 to 60",
     country: 'Country name length should be between 2 to 40 chars and contain only letters ',
     city: 'City name length should be between 2 to 40 chars and contain only letters ',
     email:' Email format should be valid',
   }
-  useEffect(() =>{
-    console.log(errorStatus[alert])
-  } ,[])
+  
   return (
     <div className={`${style.alertCard} ${errorStatus[alert] && style.displayAlert}`}>
       <div>
